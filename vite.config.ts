eaa8de85f6670521f0503f7bd8e0970319a9d68d
Ganesh Bhat <ganeshbhat@Ganeshs-MacBook-Air.local>
2026-04-26 10:22:59 +0530

    Switch GitHub Pages to branch deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const repository = process.env.GITHUB_REPOSITORY?.split('/')[1];
+const repository =
+  process.env.GITHUB_REPOSITORY?.split('/')[1] ?? 'shakti_portfolio';
 const isUserPageRepo = repository?.endsWith('.github.io');
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base:
-    process.env.NODE_ENV === 'production' && repository && !isUserPageRepo
+    process.env.NODE_ENV === 'production' && !isUserPageRepo
       ? `/${repository}/`
       : '/',
   plugins: [react()],
